f82034b54d139a95f2dd8222d3b1a5138d2a9db6
Nnaji Victor <nnajivictor0@gmail.com>
2019-11-02 18:37:22 +0100

    added skeleton for unscrambler

--- Unscrambler/Program.cs
+++ Unscrambler/Program.cs
@@ -7,7 +7,48 @@ namespace Unscrambler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool continuewordUnscrambler = true;
+            do
+            {
+
+                Console.Write("Please enter an option -F for file and M for manual: ");
+                string option = Console.ReadLine() ?? string.Empty;
+
+                switch (option.ToUpper())
+                {
+                    case "F":
+                        Console.Write("enter file name: ");
+                        ExecuteScrambledWordsinFileScenario();
+                        break;
+                    case "M":
+                        Console.Write("enter words to scramble: ");
+                        ExcecuteScrambledWordsManualEntryScenario();
+                        break;
+                    default:
+                        Console.WriteLine("Option was not recongnised");
+                        break;
+                }
+
+                var continueUnscrambling = string.Empty;
+                do
+                {
+                    Console.Write("Do you want to continue? (Y or N): ");
+                    continueUnscrambling = (Console.ReadLine() ?? string.Empty);
+
+                } while (!continueUnscrambling.Equals("Y", StringComparison.OrdinalIgnoreCase) && 
+                !continueUnscrambling.Equals("N", StringComparison.OrdinalIgnoreCase));
+
+            } while (continuewordUnscrambler);
+        }
+
+        private static void ExcecuteScrambledWordsManualEntryScenario()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ExecuteScrambledWordsinFileScenario()
+        {
+            throw new NotImplementedException();
         }
     }
 }
